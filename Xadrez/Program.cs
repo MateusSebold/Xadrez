f96043ded377dad569e05c4aaf6f61a02c01d670
MateusSebold <mateusasebold2@gmail.com>
2023-05-28 10:16:45 -0300

    28/05/2023

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -10,14 +10,9 @@ namespace Xadrez
             {
 
             Tabuleiro tabuleiro = new Tabuleiro(8,8);
-            tabuleiro.colocarPeca(new Rei(tabuleiro,Cor.Branca),new Posicao(0,3));
-            tabuleiro.colocarPeca(new Rei(tabuleiro,Cor.Preta),new Posicao(7,3));
             tabuleiro.colocarPeca(new Torre(tabuleiro,Cor.Branca),new Posicao(0,0));
-            tabuleiro.colocarPeca(new Torre(tabuleiro,Cor.Branca),new Posicao(0,7));
-            tabuleiro.colocarPeca(new Torre(tabuleiro,Cor.Preta),new Posicao(7,0));
-            tabuleiro.colocarPeca(new Torre(tabuleiro,Cor.Preta),new Posicao(7,7));
-            tabuleiro.colocarPeca(new Torre(tabuleiro,Cor.Preta), new Posicao(0,0));
-            Tela.imprimirTabuleiro(tabuleiro);
+            tabuleiro.colocarPeca(new Torre(tabuleiro,Cor.Preta),new Posicao(1,3));
+            Tela.imprimirTabuleiro(tabuleiro); 
             Console.ReadLine();
             }
             catch(Exception ex)
